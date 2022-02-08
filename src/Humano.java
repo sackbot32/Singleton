f6d7b713d22ec41f509dca4952cab94be83f6860
Pablo Soler <sackbot32@gmail.com>
2022-02-08 09:08:16 +0100

    version 2

--- src/Humano.java
+++ src/Humano.java
@@ -3,19 +3,31 @@ public class Humano {
 	 
 	private String nombre;
 	private int edad;
+	private static Humano humano;
 	
 	public String getNombre() {
 		return nombre;
 	}
-	public void setNombre(String nombre) {
+	private void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 	public int getEdad() {
 		return edad;
 	}
-	public void setEdad(int edad) {
+	private void setEdad(int edad) {
 		this.edad = edad;
 	}
+	
+	public static Humano getInstance() {
+		if(humano == null) {
+			humano = new Humano();
+			humano.setEdad(20);
+			humano.setNombre("Alberto");
+			}
+			return humano;
+	}
+	
+	
 	
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(SingletonHumano.getHumano()); 
+		System.out.println(Humano.getInstance()); 
+		System.out.println(new Humano().getInstance());
 
 	}
 
